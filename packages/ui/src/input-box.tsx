7ff356ef60cc03e2c4cb38756b7dfd68627d4e30
sujalmaiti <sujalmaiti123456@gmail.com>
2024-03-19 02:19:40 +0530

    Added React Project in monorepo

---
+++ packages/ui/src/input-box.tsx
@@ -0,0 +1,9 @@
+export const InputBox = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      hi there
+      <input type={"text"}></input>
+      hi there
+    </div>
+  );
+};
